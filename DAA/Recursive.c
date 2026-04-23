219054c3b9c48b903975db1b2d15270d23c052c0
Dheeraj Guptha <11249a166@kanchiuniv.ac.in>
2026-04-23 14:23:32 +0530

    Update Recursive.c

--- DAA/Recursive.c
+++ DAA/Recursive.c
@@ -1,19 +1,46 @@
-#include<stdio.h>
-int fib(int n) 
-{   
-  if (n <= 1) 
-  { 
-    return n; 
-  }   
-  return fib(n - 1) + fib(n - 2); 
+// Fibonacci term using Recursion
+
+/*
+AIM : To write a program to find the nth Fibonacci term using   recursion
+*/
+
+//Program
+
+#include <stdio.h>
+
+/*
+ * Function: fib
+ * Description: Computes the nth Fibonacci term using recursion.
+ * Complexity: O(2^n) - due to overlapping subproblems.
+ * @param n: index of the Fibonacci term
+ * @return: Fibonacci value at index n
+ */
+int fib(int n)
+{
+    // Base condition
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    // Recursive step
+    return fib(n - 1) + fib(n - 2);
 }
-int main()  { 
-  int n, result = 0;  
-  if (n != 1 || n < 0) 
-  { 
-    printf("Invalid input\n"); 
-    return 1; 
-  } 
-  printf("\nNth Fibonacci term = %d\n", fib(n)); 
-  return 0; 
-} 
+
+int main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    
+    // Validate input
+    if (scanf("%d", &n) != 1 || n < 0)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    printf("Nth Fibonacci term = %d\n", fib(n));
+
+    return 0;
+}
